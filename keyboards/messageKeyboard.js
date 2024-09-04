1c8aa16fe293597e32cb6770a0c316dd216dac64
Jon Snow <sn00v@proton.me>
2024-09-04 18:08:11 +0500

    refactor(message): removed save to card chat

--- keyboards/messageKeyboard.js
+++ keyboards/messageKeyboard.js
@@ -1,14 +1,5 @@
 import { InlineKeyboard } from "grammy";
-import { isSNW, isRUB, isDeposit } from "../constants/index.js";
 
-export function messageKeyboard(conversation) {
-	const { operator, direction, currency } = conversation.session;
-
-	let data = "saveMessage";
-
-	if (isSNW(operator) && isDeposit(direction) && isRUB(currency)) {
-		data = "saveCardMessage";
-	}
-
-	return new InlineKeyboard().text("Сохранить", data);
+export function messageKeyboard() {
+    return new InlineKeyboard().text("Сохранить", "saveMessage");
 }
